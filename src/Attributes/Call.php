dde632e0cfdf739e4bd6655e36f8e6e7c6004e33
isaque.santos <isaque.santos@setuptecnologia.com.br>
2025-07-31 13:03:29 -0300

    feat(#28): add process attribute call

--- src/Attributes/Call.php
+++ src/Attributes/Call.php
@@ -3,6 +3,8 @@
 namespace ISQ\MAIO\Attributes;
 
 use Attribute;
+use ISQ\MAIO\Exceptions\MethodNotExistsException;
+use ReflectionClass;
 
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class Call
@@ -11,5 +13,31 @@ class Call
         public string|object $staticClassOrObjectOrFunction,
         public string|null $methodName = null,
     ) {
+    }
+
+    public function process(object $target, mixed &$value)
+    {
+        $objectOrClassOrFunction = $this->staticClassOrObjectOrFunction;
+        $methodName = $this->methodName;
+
+        $isClass = class_exists($objectOrClassOrFunction);
+        $isFunction = function_exists($objectOrClassOrFunction);
+        $isObject = is_object($objectOrClassOrFunction);
+
+        if ($isClass || $isObject) {
+            $reflection = new ReflectionClass($objectOrClassOrFunction);
+
+            if (!$reflection->hasMethod($methodName)) {
+                throw new MethodNotExistsException("Method static $methodName not found in $objectOrClassOrFunction");
+            }
+
+            $method = $reflection->getMethod($methodName);
+            $value = $method->invoke($target, $value);
+        }
+
+        if ($isFunction) {
+            $value = $objectOrClassOrFunction($value);
+        }
+
     }
 }
