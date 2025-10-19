152d939ee2d2a494e116017e0926715917d5b5b5
isaque <isaqueclaudino12@gmail.com>
2025-10-18 23:09:20 -0300

    build(#35): adjusted more csfix

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -11,6 +11,10 @@ return (new Config())
 		'@PSR1' => true,
 		'@PSR2' => true,
 		'single_quote' => ['strings_containing_single_quote_chars' => true],
+		'php_unit_method_casing' => false,
+		'ternary_to_null_coalescing' => true,
+		'standardize_not_equals' => true,
+		'not_operator_with_successor_space' => true,
 	])
 	->setIndent("\t")
 	->setLineEnding("\n")
